f77b74f4a2bbcf6f026f13a427d01ed6956ec49d
Zirlis <Zirlis@yandex.ru>
2020-11-13 21:05:01 +0700

    start

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Artemkin.HomeWork._12._11
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
+
