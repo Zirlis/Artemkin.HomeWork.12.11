8726e9125efa6c0a21960d730bdc75d2a3bd1ac4
Zirlis <Zirlis@yandex.ru>
2020-11-15 22:33:32 +0700

    Проблема с массивом

--- Program.cs
+++ Program.cs
@@ -5,6 +5,45 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Test Task1
+
+        //Task1 task1 = new Task1();
+        //task1.Start();
+
+
+        //Test Task2
+
+        //string testString;
+        //int size;
+        //do
+        //{
+        //    Console.WriteLine($"Введите размер массива");
+        //    testString = Console.ReadLine();
+        //} while (!Int32.TryParse(testString, out size) || String.IsNullOrWhiteSpace(testString));
+
+        //int startNum;
+        //do
+        //{
+        //    Console.WriteLine($"Введите начальное число");
+        //    testString = Console.ReadLine();
+        //} while (!Int32.TryParse(testString, out startNum) && !String.IsNullOrWhiteSpace(testString));
+
+        //int interval;
+        //do
+        //{
+        //    Console.WriteLine($"Введите интервал между числами");
+        //    testString = Console.ReadLine();
+        //} while (!Int32.TryParse(testString, out interval) && !String.IsNullOrWhiteSpace(testString));
+
+        //Task2 task2 = new Task2(size, startNum, interval);
+        Task2 task2 = new Task2(4, 10, 5);
+
+        int i = 0;
+        while( i < task2.TestArray.Length)
+        {
+            Console.WriteLine(task2.TestArray);
+            i++;
+        }
+        Console.ReadKey();
     }
 }
-
---
+++ Task2.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+class Task2
+{
+    //2. а) Дописать класс для работы с одномерным массивом. 
+    //Реализовать конструктор, создающий массив заданной размерности и заполняющий массив числами от начального значения с заданным шагом. 
+    //Создать свойство Sum, которые возвращают сумму элементов массива, метод Inverse, меняющий знаки у всех элементов массива, метод Multi, умножающий каждый элемент массива на определенное число, свойство MaxCount, возвращающее количество максимальных элементов. 
+    //В Main продемонстрировать работу класса.
+    //б)Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
+
+    //Часть А
+    internal int[] TestArray { get; private set; }
+
+    internal Task2 (int size, int startNum, int interval)
+    {
+        int[] array = new int[size];
+        array[0] = startNum;
+
+        int i = 1;
+        while(i < size)
+        {
+            array[i] = array[i - 1] + interval;
+            i++;
+        }
+        TestArray = array;
+    }
+
+    internal int Sum()
+    {
+        int sum = 0;
+        int i = 0;
+        while (i < TestArray.Length)
+        {
+            sum += TestArray[i];
+            i++;
+        }
+        return sum;
+    }
+
+    internal void Inverse()
+    {
+        int i = 0;
+        while (i < TestArray.Length)
+        {
+            TestArray[i] = -TestArray[i];
+            i++;
+        }
+    }
+
+    internal void Multi(int multiplier)
+    {
+        int i = 0;
+        while (i < TestArray.Length)
+        {
+            TestArray[i] *= multiplier;
+            i++;
+        }
+    }
+
+    internal int MaxCount()
+    {
+        int maxNum = TestArray[0];
+        int i = 0;
+        while (i < TestArray.Length)
+        {
+            if(TestArray[i] > maxNum)
+            {
+                maxNum = TestArray[i];
+            }
+            i++;
+        }
+
+        int maxCount = 0;
+        while (i < TestArray.Length)
+        {
+            if (TestArray[i] == maxNum)
+            {
+                maxCount++;
+            }
+            i++;
+        }
+        return maxCount;
+    }
+
+    //Часть Б
+    internal void Write(string path)
+    {
+        if (String.IsNullOrEmpty(path)) return;
+
+        StreamWriter sw = new StreamWriter(path, false, Encoding.ASCII);
+
+        int i = 0;
+        while (i < TestArray.Length)
+        {
+            sw.WriteLine(TestArray[i]);
+            i++;
+        }
+
+        sw.Flush();
+        sw.Close();
+    }
+
+    internal void Reader(string path)
+    {
+        if (String.IsNullOrEmpty(path)) return;
+
+        StreamReader sr = new StreamReader(path);
+        List<int> list = new List<int>();
+        int i = 0;
+        while (!sr.EndOfStream)
+        {
+            list[i] = Int32.Parse(sr.ReadLine());
+            i++;
+        }
+
+        int[] testArray = new int[list.Count];
+        i = 0;
+        while (i < testArray.Length)
+        {
+            testArray[i] = list[i];
+            i++;
+        }
+
+        TestArray = testArray;
+    }
+}
